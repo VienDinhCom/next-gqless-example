1569998bc49c137e8fce8eb9eefb100f52553e7a
Vien Dinh <maxvien@gmail.com>
2021-04-26 11:00:35 +0700

    micro changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,10 @@ import type { AppProps, AppContext } from 'next/app';
 import { PropsWithServerCache } from '@gqless/react';
 import { prepareReactRender, useHydrateCache, useQuery } from '../gqless';
 
-interface Props extends PropsWithServerCache<AppProps> {
-  cacheSnapshot?: string;
-}
+interface Props extends PropsWithServerCache<AppProps> {}
 
 function MyApp({ Component, pageProps, cacheSnapshot }: Props) {
-  useHydrateCache({ cacheSnapshot });
+  useHydrateCache({ cacheSnapshot, shouldRefetch: false });
 
   return <Component {...pageProps} />;
 }
