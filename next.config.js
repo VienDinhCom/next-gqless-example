0445c1c486f3bc141d2ea643ff96316c25c953e9
Vien Dinh <maxvien@gmail.com>
2021-04-26 15:32:16 +0700

    micro changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 
+// https://developers.google.com/web/tools/workbox/modules/workbox-webpack-plugin#generatesw_plugin
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
