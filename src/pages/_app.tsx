6d839bd3625597d35b5af8b3d07f64d6f264964e
Vien Dinh <maxvien@gmail.com>
2021-04-26 10:47:07 +0700

    ssr

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
-function MyApp({ Component, pageProps }) {
+import App from 'next/app';
+import type { AppProps, AppContext } from 'next/app';
+import { PropsWithServerCache } from '@gqless/react';
+import { prepareReactRender, useHydrateCache, useQuery } from '../gqless';
+
+interface Props extends PropsWithServerCache<AppProps> {
+  cacheSnapshot?: string;
+}
+
+function MyApp({ Component, pageProps, cacheSnapshot }: Props) {
+  useHydrateCache({ cacheSnapshot });
+
   return <Component {...pageProps} />;
 }
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext);
+  const { cacheSnapshot } = await prepareReactRender(<MyApp {...{ ...appContext, ...appProps }} />);
+
+  return { ...appProps, cacheSnapshot };
+};
 
 export default MyApp;
